cc2c3dc3daaec443bdfd5de12ca42eafd317dd9d
Kailari <ilari97@hotmail.com>
2018-01-01 04:04:22 +0200

    Started working on adding an event bus to the API

---
+++ src/api/java/jakojaannos/hcparty/api/PartyAPI.java
@@ -0,0 +1,9 @@
+package jakojaannos.hcparty.api;
+
+import net.minecraftforge.fml.common.eventhandler.EventBus;
+
+public class PartyAPI {
+    public static final EventBus API_EVENT_BUS = new EventBus();
+
+    private PartyAPI() {}
+}
