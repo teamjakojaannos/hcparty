588487b49070f4a7a110dc0575392a42913760a9
Kailari <ilari97@hotmail.com>
2018-05-16 16:59:33 +0300

    Added a TODO

--- src/main/java/jakojaannos/life/entity/HealthTrackerEventHandler.java
+++ src/main/java/jakojaannos/life/entity/HealthTrackerEventHandler.java
@@ -9,6 +9,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber
 public class HealthTrackerEventHandler {
+    // TODO: Might be better performance-wise to track health at living damage events and reset at entity constructing
+    //      + avoids getting tracking getting called for all living entities instead every tick
+    //      - breaks if mods cause damage that isn't tracked by living damage event
+    //          -> (could be "fixed" by providing configurable option to enable this)
+    //          -> (works by conditionally registering two separate handlers directly at init => option requires MC restart)
     @SubscribeEvent
     public static void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
         EntityLivingBase entity = event.getEntityLiving();
